5e4cbd887933396d126cf1146ec066ea27c986ba
Kuroakai <derudj12345@gmail.com>
2025-03-09 21:38:26 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmodul4_103022300101/KodePos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103022300101
+{
+    class KodePos
+    {
+        //enum untuk daftar kelurahan
+        public enum kelurahan { Batununggal, Kujangsari, Mengger, Wates, Cijaura, Jatisari, MargaSari, Sekejati, Kebonwaru, Maleer, Samoja };
+
+        //Metode untuk mendapatkan kode pos kelurahan
+        public static int getKodePos(kelurahan Kelurahan)
+        {
+            int[] kodePos = { 40266, 40287, 40267, 40256, 40287, 48286, 48286, 40286, 48272, 40274, 40273 }; //Array berisi data kode pos kelurahan
+            return kodePos[(int)Kelurahan]; //mengembalikan nilai indeks sesuai enum yang ada di parameter kelurahan
+        }
+    }
+}
--- tpmodul4_103022300101/Program.cs
+++ tpmodul4_103022300101/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul4_103022300101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Daftar Kelurahan");
+            Console.WriteLine("=================================");
+            Console.WriteLine("Kelurahan\t |     Kode Pos");
+            Console.WriteLine("=================================");
+            foreach (KodePos.kelurahan kel in Enum.GetValues(typeof(KodePos.kelurahan))) //Perulangan untuk menampilkan kelurahan dan kodepos
+            {
+                String namaKelurahan = kel.ToString();
+                Console.WriteLine($"{kel,-16} |     {KodePos.getKodePos(kel)}");
+            }
+        }
+    }
+}
